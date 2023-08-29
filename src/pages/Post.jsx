e34229e3db68ceb440466127beefe0b843da5a51
ShamiulIslam388 <shamiulislam388@gmail.com>
2023-08-30 00:16:05 +0600

    use promise all to fetch data from multiple url

--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -4,27 +4,47 @@ import { Link, useParams } from "react-router-dom";
 const Post = () => {
   const { id } = useParams();
   const [post, setPost] = useState({});
+  const [user, setUser] = useState({});
   const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     setLoading(true);
-    fetch(`https://jsonplaceholder.typicode.com/posts/${id}`)
-      .then((res) => res.json())
-      .then((data) => {
-        setPost(data);
-        document.title = data.title;
+
+    Promise.all([
+      fetch(`https://jsonplaceholder.typicode.com/posts/${id}`).then((res) =>
+        res.json()
+      ),
+      fetch(`https://jsonplaceholder.typicode.com/users/${post?.userId}`).then(
+        (res) => res.json()
+      ),
+    ])
+      .then(([postData, userData]) => {
+        setPost(postData);
+        setUser(userData);
+        document.title = postData.title;
       })
       .catch((err) => console.log(err))
       .finally(() => setLoading(false));
-  }, []);
+  }, [id, post?.userId]);
 
   const content = loading ? (
     <div>Loading...</div>
   ) : (
-    <div>
+    <div className="p-4 border rounded-md">
       <div className="text-xl font-semibold">{post.title}</div>
       <div className="text-sm font-medium text-gray-500 max-w-[120ch] break-word">
         {post.body}
+      </div>
+      <div className="px-5 py-1">
+        <div className="text-md font-semibold">Who wrote the post?</div>
+        <div className="px-4">
+          {" "}
+          <div>Name: {user?.name}</div>
+          <div>Email: {user?.email}</div>
+          <div>Address: {user?.address?.city}</div>
+          <div>Website: {user?.website}</div>
+          <div>Mobile: {user?.phone}</div>
+        </div>
       </div>
       <Link to="/posts" className="underline text-blue-500">
         Go to posts
--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const Posts = () => {
   const [posts, setPosts] = useState([]);
-
+  const navigate = useNavigate();
   useEffect(() => {
     document.title = "posts page";
     fetch("https://jsonplaceholder.typicode.com/posts")
@@ -13,15 +13,22 @@ const Posts = () => {
   }, []);
 
   return (
-    <div className="flex flex-col underline">
-      {posts.slice(0, 10).map((post) => (
-        <Link
-          to={`/posts/${post.id}`}
+    <div className="flex flex-wrap gap-2.5 ">
+      {posts.slice(0, 20).map((post) => (
+        <div
           key={post.id}
-          className="text-xl font-semibold"
+          className="border p-4 rounded-md w-[200px] flex flex-col justify-between shadow"
         >
-          {post.title}
-        </Link>
+          <div className="text-xl font-500 text-sm">
+            {post.title.slice(0, 20)}...
+          </div>
+          <button
+            onClick={() => navigate(`/posts/${post.id}`)}
+            className="max-w-fit self-end text-xs text-white bg-blue-500 border-0 px-2 py-1 rounded-md mt-2.5 hover:bg-blue-400 hover:scale-90 transition-all duration-300"
+          >
+            View Details
+          </button>
+        </div>
       ))}
     </div>
   );
